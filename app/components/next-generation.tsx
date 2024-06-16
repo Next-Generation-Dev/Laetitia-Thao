b8612a744d29da0081888386351db55448efda2c
Meymoun Boualaoui <77726988+MeymounB@users.noreply.github.com>
2024-06-16 23:47:39 +0200

    style(components): small adjustements

--- app/components/next-generation.tsx
+++ app/components/next-generation.tsx
@@ -5,7 +5,7 @@ const NextGeneration: React.FC = () => {
   return (
     <div className=" text-black text-small text-center">
       <p>
-        Site web conceptualisé et développé par{" "}
+        Site web conceptualisé et développé par <br className="ph:hidden" />
         <Link
           href="https://next-generation.dev"
           className="text-default-700 hover:text-gray-700 duration-300"
