e8e957273f9f6bf0e45c7023df04d70098dd49bb
Meymoun Boualaoui <77726988+MeymounB@users.noreply.github.com>
2024-06-16 23:08:30 +0200

    feat(meta): add og img

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,23 @@ import "@/globals.scss";
 import { Providers } from "@/providers";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
+import ogcool from "ogcool";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Laetitia Thao - Assistante maternelle Agréée",
   description: "Laetitia Thao - Assistante maternelle Agréée",
+  openGraph: {
+    images: [
+      ogcool("Branding 5", {
+        modifications: [
+          { name: "Text", text: "Laetitia Thao" },
+          { name: "Image", src: "@/assets/img1.png" },
+        ],
+      }),
+    ],
+  },
 };
 
 export default function RootLayout({
